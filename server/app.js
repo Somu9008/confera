d96932193a689f8c75f6492066a95cec54493ff0
somu9008 <sharathg9008@gmail.comm>
2025-06-28 11:48:54 +0530

    clint url added

--- server/app.js
+++ server/app.js
@@ -24,14 +24,14 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://confera-91ss.vercel.app/",
     credentials: true,
   },
 });
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://confera-91ss.vercel.app/",
     credentials: true,
   })
 );
